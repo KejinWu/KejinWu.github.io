60fc3504e10668c630f6e82d3b00405e9827424e
KKKKKJ <kwu@ucsd.edu>
2026-04-30 22:56:04 -0700

    changetimeline

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1777604722',resources: [
+    cacheName: 'chirpy-1777614920',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
@@ -10,7 +10,7 @@ const swconf = {
       
         '/Students/',
       
-        '/Timeline/',
+        '/timeline/',
       
 
       
