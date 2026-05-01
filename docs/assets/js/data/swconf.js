3f5c2cc00d049179d837ba53620c13e5d321b060
KKKKKJ <kwu@ucsd.edu>
2026-04-30 19:51:40 -0700

    changehometitle

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1777603320',resources: [
+    cacheName: 'chirpy-1777603866',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
