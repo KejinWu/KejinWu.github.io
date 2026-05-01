b5a7a7735b2af6c05d696efdd8ff3f9b992cf512
KKKKKJ <kwu@ucsd.edu>
2026-04-30 20:06:04 -0700

    changetitletap

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1777604199',resources: [
+    cacheName: 'chirpy-1777604722',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
@@ -10,7 +10,7 @@ const swconf = {
       
         '/Students/',
       
-        '/timeline/',
+        '/Timeline/',
       
 
       
