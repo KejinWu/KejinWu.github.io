bb6a4b220cb59db16eaa0c2362191a2a245ddcaa
KKKKKJ <kwu@ucsd.edu>
2026-04-30 19:42:55 -0700

    changetopbar

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1777602887',resources: [
+    cacheName: 'chirpy-1777603320',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
