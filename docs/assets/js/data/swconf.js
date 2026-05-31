be28c957bd08456cfce913e564f95d14141bf3ed
KKKKKJ <kwu@ucsd.edu>
2026-05-31 13:15:34 -0500

    addness

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1777619092',resources: [
+    cacheName: 'chirpy-1780251279',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
